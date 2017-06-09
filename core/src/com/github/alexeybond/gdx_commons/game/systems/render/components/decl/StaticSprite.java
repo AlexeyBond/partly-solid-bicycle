a413a789b60d11d051244962b4bde0eecc608cad
A.Bond <alexey.bond.94.55@gmail.com>
2017-06-09 14:25:58 +0600

    Add alternative sprite component that uses transform matrix to compute coordinates more accurately.

--- core/src/com/github/alexeybond/gdx_commons/game/systems/render/components/decl/StaticSprite.java
+++ core/src/com/github/alexeybond/gdx_commons/game/systems/render/components/decl/StaticSprite.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.github.alexeybond.gdx_commons.game.Component;
 import com.github.alexeybond.gdx_commons.game.declarative.ComponentDeclaration;
 import com.github.alexeybond.gdx_commons.game.declarative.GameDeclaration;
+import com.github.alexeybond.gdx_commons.game.systems.render.components.AccurateStaticSpriteComponent;
 import com.github.alexeybond.gdx_commons.game.systems.render.components.StaticSpriteComponent;
 import com.github.alexeybond.gdx_commons.ioc.IoC;
 
@@ -19,9 +20,16 @@ public class StaticSprite implements ComponentDeclaration {
 
     public float scale = 1f;
 
+    /** If true compute sprite coordinates more accurate way */
+    public boolean accurate = false;
+
     @Override
     public Component create(GameDeclaration gameDeclaration) {
         if (null == region) region = IoC.resolve("get texture region", sprite);
-        return new StaticSpriteComponent(pass, region, scale);
+        if (accurate) {
+            return new AccurateStaticSpriteComponent(pass, region, scale);
+        } else {
+            return new StaticSpriteComponent(pass, region, scale);
+        }
     }
 }
