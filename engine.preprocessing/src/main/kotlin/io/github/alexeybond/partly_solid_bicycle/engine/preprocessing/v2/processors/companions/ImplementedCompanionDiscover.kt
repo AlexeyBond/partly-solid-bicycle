22e20e620cdb954ec067e89fba8f1408e4af50f8
A.Bond <alexey.bond.94.55@gmail.com>
2018-05-06 19:33:46 +0600

    Add registration of pre-implemented companions. All tests are passed.

---
+++ engine.preprocessing/src/main/kotlin/io/github/alexeybond/partly_solid_bicycle/engine/preprocessing/v2/processors/companions/ImplementedCompanionDiscover.kt
@@ -0,0 +1,74 @@
+package io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.v2.processors.companions
+
+import com.squareup.javapoet.ClassName
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.annotation_processors.getAnnotationMirror
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.annotation_processors.getListValue
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.annotation_processors.getValue
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.annotations.ComponentCompanion
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.interfaces.context.ItemContext
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.interfaces.processor.ItemProcessor
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.v2.process
+import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.element.AnnotationMirror
+import javax.lang.model.element.TypeElement
+import javax.lang.model.type.DeclaredType
+import javax.tools.Diagnostic
+
+class ImplementedCompanionDiscover : ItemProcessor {
+    override fun getPriority(): Int {
+        return Int.MAX_VALUE
+    }
+
+    override fun acceptsItemKind(itemKind: String): Boolean {
+        return "round" == itemKind
+    }
+
+    override fun processItem(context: ItemContext): Boolean {
+        val roundEnv: RoundEnvironment = context["roundEnv"]
+        val pCtx = context.context
+        val pEnv = pCtx.env
+
+        roundEnv.getElementsAnnotatedWith(ComponentCompanion::class.java)
+                .forEach { elem ->
+                    val annotation: AnnotationMirror = elem
+                            .getAnnotationMirror(pEnv, ComponentCompanion::class)!!
+                    val type: String = annotation
+                            .getValue(pEnv.elementUtils, "companionType")
+                            .value as String
+                    val componentTM: DeclaredType = annotation
+                            .getValue(pEnv.elementUtils, "component")
+                            .value as DeclaredType
+                    val envs: List<String> = annotation
+                            .getValue(pEnv.elementUtils, "env")
+                            .getListValue()
+                    val componentCNS = (componentTM.asElement() as TypeElement).qualifiedName
+
+                    val componentCtx = try {
+                        pCtx.getItem("component:$componentCNS")
+                    } catch (e: NoSuchElementException) {
+                        pEnv.messager.printMessage(
+                                Diagnostic.Kind.ERROR,
+                                "Class passed as component is not a component",
+                                elem, annotation, annotation.getValue(pEnv.elementUtils, "component")
+                        )
+
+                        return@forEach
+                    }
+
+                    val componentCN = componentCtx.get<ClassName>("className")
+                    val companionCN = ClassName.get(elem as TypeElement)
+
+                    pCtx.process("companion:$type:$componentCN:preImplemented:$companionCN") {
+                        set("kind", "component-companion")
+
+                        set("companionType", type)
+                        set("component", componentCtx)
+                        set("envs", envs)
+                        set("className", companionCN)
+                        set("implClassName", companionCN)
+                    }
+                }
+
+        return false
+    }
+}
