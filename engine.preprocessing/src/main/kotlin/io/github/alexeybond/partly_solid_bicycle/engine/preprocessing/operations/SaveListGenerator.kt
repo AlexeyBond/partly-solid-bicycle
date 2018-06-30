532e69feddb1a346b83c76c93e8baedaab6929b7
A.Bond <alexey.bond.94.55@gmail.com>
2018-06-30 14:28:40 +0600

    Add "saver" companion generation for list and array properties.

---
+++ engine.preprocessing/src/main/kotlin/io/github/alexeybond/partly_solid_bicycle/engine/preprocessing/operations/SaveListGenerator.kt
@@ -0,0 +1,81 @@
+package io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.operations
+
+import com.squareup.javapoet.CodeBlock
+import io.github.alexeybond.partly_solid_bicycle.core.interfaces.data.OutputDataObject
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.interfaces.context.ProcessingContext
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.interfaces.operations.PropertyOperationGenerator
+import io.github.alexeybond.partly_solid_bicycle.engine.preprocessing.interfaces.properties.PropertyInfo
+import javax.lang.model.type.ArrayType
+import javax.lang.model.type.DeclaredType
+import javax.lang.model.type.TypeKind
+import javax.lang.model.type.TypeMirror
+import javax.tools.Diagnostic
+
+class SaveListGenerator : PropertyOperationGenerator {
+    /*
+    lvalue: ODO
+    rvalue: Iterable<T> | T[]
+
+    for (T val: rvalue) {
+        ODO data = lvalue.addItem();
+
+        {save val to data}
+    }
+     */
+    override fun generateOperation(
+            operation: String,
+            propertyInfo: PropertyInfo,
+            processingContext: ProcessingContext,
+            root: PropertyOperationGenerator,
+            args: MutableMap<String, Any>): CodeBlock? {
+        if ("save" != operation) return null
+
+        val type = args["type"] as TypeMirror
+
+        val penv = processingContext.env
+        val tu = penv.typeUtils
+        val eu = penv.elementUtils
+
+        val elemType: TypeMirror
+
+        when {
+            tu.isAssignable(tu.erasure(type), eu.getTypeElement("java.lang.Iterable").asType()) -> {
+                val typeArgs = (type as DeclaredType).typeArguments
+                if (typeArgs.size != 1) {
+                    penv.messager.printMessage(
+                            Diagnostic.Kind.WARNING,
+                            "Type $type seems to be a list but has no type parameters.",
+                            propertyInfo.declaringElements[0]
+                    )
+                    return null
+                }
+                elemType = typeArgs[0]
+            }
+            type.kind == TypeKind.ARRAY -> elemType = (type as ArrayType).componentType
+            else -> return null
+        }
+
+        val valLocal = makeLocalName("val")
+        val dataLocal = makeLocalName("data")
+
+        val code = root.generateOperation(
+                "save", propertyInfo, processingContext, root,
+                mapOf(
+                        "type" to elemType,
+                        "lvalue" to dataLocal,
+                        "rvalue" to valLocal
+                )
+        ) ?: return null
+
+        val cb = CodeBlock.builder()
+
+        cb.run {
+            beginControlFlow("for (\$T $valLocal : ${args["rvalue"]})", elemType)
+            add("\$T $dataLocal = ${args["lvalue"]}.addItem();\n", OutputDataObject::class.java)
+            add(code)
+            endControlFlow()
+        }
+
+        return cb.build()
+    }
+}
--- test/annotation_processing/src/test/java/io/github/alexeybond/partly_solid_bicycle/test/annotation_processing/SaverCompanionTest.java
+++ test/annotation_processing/src/test/java/io/github/alexeybond/partly_solid_bicycle/test/annotation_processing/SaverCompanionTest.java
@@ -1,12 +1,13 @@
 package io.github.alexeybond.partly_solid_bicycle.test.annotation_processing;
 
-import generated.io.github.alexeybond.partly_solid_bicycle.test.annotation_processing.Component1$_impl;
-import generated.io.github.alexeybond.partly_solid_bicycle.test.annotation_processing.Component1_saver;
+import generated.io.github.alexeybond.partly_solid_bicycle.test.annotation_processing.*;
 import io.github.alexeybond.partly_solid_bicycle.core.impl.data.dynamic.DynamicNode;
 import io.github.alexeybond.partly_solid_bicycle.core.impl.data.helpers.matching.DataMatcher;
 import io.github.alexeybond.partly_solid_bicycle.test.annotation_processing.test_utils.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class SaverCompanionTest {
     private DynamicNode data;
@@ -39,6 +40,10 @@ public class SaverCompanionTest {
         cmp.y = 48;
         cmp.z = 13;
 
+        cmp.name = "Vasya";
+
+        cmp.data = TestUtils.parseJSON("{__holy__:cow}");
+
         Component1_saver
                 .RESOLVER
                 .resolve(cmp)
@@ -46,7 +51,39 @@ public class SaverCompanionTest {
 
         DataMatcher.assertMatch(
                 data,
-                TestUtils.parseJSON("{x: 32, y: 48, z: 13}")
+                TestUtils.parseJSON("{x: 32, y: 48, z: 13, name: Vasya, data: {__holy__:cow}}")
+        );
+    }
+
+    @Test
+    public void testLists() {
+        Component2$_impl cmp = new Component2$_impl();
+
+        cmp.multilist = Arrays.asList(
+                Arrays.asList("hello", "world"),
+                Arrays.asList("hail", "Satan", "!")
+        );
+
+        Component2_saver.RESOLVER.resolve(cmp).save(cmp, data);
+
+        DataMatcher.assertMatch(
+                data,
+                TestUtils.parseJSON("{multilist:[[hello,world],[hail,Satan,!]]}")
+        );
+    }
+
+    @Test
+    public void testArrays() {
+        Component3$_impl cmp = new Component3$_impl();
+
+        cmp.floats = new float[]{3.125f, 42, 21};
+        cmp.ints = new int[][][]{{{1, 2, 3}, {4}}, {{5}}, {{42}}};
+
+        Component3_saver.RESOLVER.resolve(cmp).save(cmp, data);
+
+        DataMatcher.assertMatch(
+                data,
+                TestUtils.parseJSON("{floats:[3.125,42,21],ints:[[[1,2,3],[4]],[[5]],[[42]]]}")
         );
     }
 }
